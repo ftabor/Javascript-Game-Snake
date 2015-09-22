e17df130198f202057891a987138948253088d95
ftabor <ftaborconsulting@gmail.com>
2015-09-22 16:30:15 +0100

    Data structure of the gaem

--- snake.js
+++ snake.js
@@ -24,10 +24,11 @@ var grid = {
 	},
 
 	set: function(val, x, y) {
-
+			this._grid[x][y] = val;
 	},
 
 	get: function(x, y) {
+		return this._grid[x][y];
 
 	}
 }
@@ -35,24 +36,37 @@ var grid = {
 var snake = {
 
 	direction: null,
+	last: 		 null,
 	_queue: 	 null, 
 
 	init: function(d, x, y) {
+		this.direction = d;
 
+		this._queue = [];
+		this.insert(x, y);
 	},
 
 	insert: function(x, y) {
-
+		this._queue.unshift({x:x, y:y});
+		this.last = this._queue[0];
 	},
 
 	remove: function() {
-
+		return this._queue.pop();
 	}
-
 }
 
 function setFood() {
-
+	var empty = [];
+	for (var x=0; x < grid.width; x++) {
+		for (var y=0; y < grid.height; y++) {
+			if (grid.get(x, y) === EMPTY) {
+				empty.push(x:x, y:y});
+			}
+		}
+	}
+	var randpos = empty[Math.floor(Math.random()*empty.length)];
+	grid.set(FRUIT, randpos.x, randpos.y) 
 }
 
 function main() {
