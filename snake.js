de49bd4e7d14cf52cacd5b9ba0c0168b9207d486
ftabor <ftaborconsulting@gmail.com>
2015-09-22 15:52:01 +0100

    var Grid

---
+++ snake.js
@@ -0,0 +1,25 @@
+//Constants
+var COLS=26, ROWS=26;
+
+//IDs
+var EMPTY=0, SNAKE=1, FRUIT=2;
+
+var grid = {
+
+	width:  null
+	height: null, 
+	_grid: 	null,
+
+	init: function(d, c, r) {
+
+	},
+
+	set: function(val, x, y) {
+
+	},
+
+	get: function(x, y) {
+		
+	}
+}
+
